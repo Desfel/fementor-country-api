2ae4251ab47d1172bc4f04c87701358c3c586a3a
Desfel <wil.southiphonh@gmail.com>
2021-11-24 22:21:33 -0500

    :tada: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <nav-bar></nav-bar>
+    <div class="main-wrapper">
+      <router-view />
+    </div>
+
+    <new-content-available-toastr
+      v-if="newContentAvailable"
+      class="new-content-available-toastr"
+      :refreshing-app="refreshingApp"
+      @refresh="serviceWorkerSkipWaiting"
+    ></new-content-available-toastr>
+    <apple-add-to-home-screen-modal
+      v-if="showAddToHomeScreenModalForApple"
+      class="apple-add-to-home-screen-modal"
+      @close="closeAddToHomeScreenModalForApple(false)"
+    >
+    </apple-add-to-home-screen-modal>
+  </div>
+</template>
+<script>
+import NavBar from '@/components/NavBar'
+import NewContentAvailableToastr from '@/components/NewContentAvailableToastr'
+import AppleAddToHomeScreenModal from '@/components/AppleAddToHomeScreenModal'
+import { mapState, mapActions, mapGetters } from 'vuex'
+
+export default {
+  components: { NavBar, NewContentAvailableToastr, AppleAddToHomeScreenModal },
+  computed: {
+    ...mapGetters('app', ['newContentAvailable']),
+    ...mapState('app', ['showAddToHomeScreenModalForApple', 'refreshingApp'])
+  },
+  methods: mapActions('app', [
+    'closeAddToHomeScreenModalForApple',
+    'serviceWorkerSkipWaiting'
+  ])
+}
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+
+  a {
+    font-weight: 500;
+    text-decoration: none;
+  }
+
+  #app {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 16px;
+    color: #2c3e50;
+
+    .new-content-available-toastr {
+      position: absolute;
+      bottom: 10px;
+      right: 10px;
+    }
+
+    .main-wrapper {
+      .page-wrapper {
+        width: 100%;
+        height: 100%;
+        min-height: 100vh;
+        margin: auto;
+      }
+    }
+  }
+}
+</style>
